ca625d0ae6b6865392dcde1aa777bf4160f4131c
Tryonme <ruben.espinar.10@icloud.com>
2025-08-15 23:40:28 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('MAIL_TO', 'contacto@tryonyou.app'); // cambia por tu correo de recepción
+define('MAIL_FROM', 'no-reply@tryonyou.app');
+define('MAKE_WEBHOOK_URL', '');
+define('ENABLE_MAIL', true);
+define('ENABLE_MAKE', false);
+?>
