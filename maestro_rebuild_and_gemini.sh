1d7c53c054277656f6925fb52cca3ace5fa227cb
Tryonme <ruben.espinar.10@icloud.com>
2025-12-01 21:23:05 +0100

    Add maestro_rebuild_and_gemini.sh script

---
+++ maestro_rebuild_and_gemini.sh
@@ -0,0 +1,7 @@
+maestro_rebuild_and_gemini.sh
+cd ~
+nano maestro_rebuild_and_gemini.sh
+# pegas el código, guardas y sales
+chmod +x maestro_rebuild_and_gemini.sh
+zsh maestro_rebuild_and_gemini.sh
+
