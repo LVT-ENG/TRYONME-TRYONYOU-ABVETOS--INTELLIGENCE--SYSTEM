aa4f409a86b855821e37f8c6d4fa42b6462b799b
LVT-ENG <214667862+LVT-ENG@users.noreply.github.com>
2025-12-19 16:30:16 -0500

    RESTORE: Full pilot with 360 avatar rotation, complete look builder, interactive demo

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+// Pages
+import Home from './pages/Home'
+import Brands from './pages/Brands'
+import MyAvatar from './pages/MyAvatar'
+import Wardrobe from './pages/Wardrobe'
+import Showroom from './pages/Showroom'
+import GlowUp from './pages/GlowUp'
+import Demo from './pages/Demo'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-tryonyou-black">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/brands" element={<Brands />} />
+          <Route path="/my-avatar" element={<MyAvatar />} />
+          <Route path="/wardrobe" element={<Wardrobe />} />
+          <Route path="/showroom" element={<Showroom />} />
+          <Route path="/glow-up" element={<GlowUp />} />
+          <Route path="/demo" element={<Demo />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
