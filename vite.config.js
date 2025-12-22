7cb9913a2bcb9a885c2513b363fa39699a2c0bfc
Ruben Espinar <ruben.espinar10@icloud.com>
2025-12-22 01:57:09 +0100

    CLEANUP: Removing artifacts and updating vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/TRYONME-TRYONYOU-ABVETOS--INTELLIGENCE--SYSTEM/',
+  base: '/', 
+  build: {
+    outDir: 'dist',
+  }
 })
