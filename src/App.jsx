ed10e94bbe513283f0a3baf7eb6afe91096ee54d
Ruben Espinar <ruben.espinar10@icloud.com>
2025-12-31 14:12:32 +0100

    fix: total reset of entry points to solve blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,9 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Forzamos que la raíz sea Investors */}
         <Route path="/" element={<Investors />} />
         <Route path="/investors" element={<Investors />} />
         <Route path="/MagicMirror" element={<MagicMirror />} />
-        {/* Cualquier ruta desconocida redirige a la home para evitar pantalla negra */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
